17e28ab55f7759068b0f0c152a0751d12f39f10c
Mojtaba Mohammadi <mhmojtaba@gmail.com>
2022-10-28 22:31:23 +0330

    change bgColor pallete

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function changeColor(id) {
+  //   const a = document.getElementById(id).innerHTML;
+  //   console.log(a);
+  document.body.style.background = document.getElementById(id).innerHTML;
+}
